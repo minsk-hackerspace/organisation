4817a1a9655dad912114ab83a42d86f805119891
Yury Adamov <begemotv2718@gmail.com>
2014-12-05 16:56:45 +0300

    Some photos

---
+++ presentations/partyhard_20141205/imget.sh
@@ -0,0 +1,2 @@
+curl https://scontent-b-vie.xx.fbcdn.net/hphotos-xap1/t31.0-8/1780084_400909660068071_3354848939429910024_o.jpg?dl=1 > ./hackerspace_view.jpg
+curl http://hackerspace.by/images/f.jpg > ./f.jpg
